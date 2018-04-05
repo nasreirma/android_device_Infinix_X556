addd06c205fbb945352b9672bd11cb49a7df2daa
Nasrellah Irhmar <nasreirma@gmail.com>
2018-04-05 14:18:34 +0100

    update FP blobs and some fixes

--- device_X556.mk
+++ device_X556.mk
@@ -160,7 +160,10 @@ PRODUCT_PACKAGES += \
 # Fingerprint HAL
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
-	fingerprintd
+	
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
 
 # Power
 PRODUCT_PACKAGES += \
