61ab00dd3b3f9d9111af78912e7e9f857b36af94
nasreirma <nasreirma@gmail.com>
2018-05-03 04:50:57 +0100

    add keymaster service

--- device_X556.mk
+++ device_X556.mk
@@ -189,7 +189,8 @@ PRODUCT_PACKAGES += \
     
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl\
+    android.hardware.keymaster@3.0-service
     
 # Sensors HAL
 PRODUCT_PACKAGES += \
